1cae8920234643d38dc6b413fe3e2bbba0cf36d1
Amaan Chaudhary <138295590+Amaanchaudhary@users.noreply.github.com>
2024-08-22 04:34:42 +0000

    12 nums done math remaining

--- 12_nums_and_math.js
+++ 12_nums_and_math.js
@@ -1,2 +1,17 @@
-const score = 400
-console.log(score)
+const score = 400;
+console.log(score);
+
+const balance = new Number(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 12.3155;
+// console.log(otherNumber.toPrecision(3));
+
+const otherNumber2 = 1000000;
+// console.log(otherNumber2.toLocaleString('en-IN'))
+
+//++++++++++++Maths++++++++++++
+
